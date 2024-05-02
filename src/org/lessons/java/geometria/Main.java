7a8b4eb07000849c95bcf8d85c6a4a174936ee01
LudovicaMassimino <ludovica.massimino93@gmail.com>
2024-05-02 13:04:01 +0200

    incapsulamento divisi in step

--- src/org/lessons/java/geometria/Main.java
+++ src/org/lessons/java/geometria/Main.java
@@ -2,27 +2,31 @@ package org.lessons.java.geometria;
 import java.util.Scanner;
 
 public class Main {
-
+	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
+		//prompt in java
 		System.out.print("Inserisci la base del rettangolo: ");
 		int base = input.nextInt();
 		
 		System.out.print("Inserisci l'altezza del rettangolo: ");
 		int altezza = input.nextInt();
 		
-		Rettangolo rettangolo = new Rettangolo(base, altezza);
+		//istanza: creo un nuovo rettangolo con i valori inseriti dall'utente
+		Rettangolo mioRettangolo = new Rettangolo(base, altezza);
 		
-		int area = rettangolo.calcolaArea();
-		int perimetro = rettangolo.calcolaPerimetro();
+		int area = mioRettangolo.calcolaArea();
+		int perimetro = mioRettangolo.calcolaPerimetro();
 		
 		System.out.println("Base: " + base );
 		System.out.println("Altezza: " + altezza);
-		System.out.println("Perimetro: " + perimetro);
-		System.out.println("Area: " + area);
+		System.out.println("Perimetro: " + perimetro); // sarebbe mioRettangolo.calcolaPerimetro()
+		System.out.println("Area: " + area); // sarebbe mioRettangolo.calcolaArea()
 
-        rettangolo.disegna();
+        mioRettangolo.disegna();
+        
+        input.close();
 	}
 
 }
--- src/org/lessons/java/geometria/Rettangolo.java
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -2,24 +2,44 @@ package org.lessons.java.geometria;
 
 public class Rettangolo {
 	
-	int base;
-	int altezza;
+	//1° step incapsulamento: inserisco gli attributi private
+	private int base;
+	private int altezza;
 	
+	//costruttore Rettangolo per inizializzare base e altezza
 	public Rettangolo(int base, int altezza) {
 		this.base = base;
 		this.altezza = altezza;
 	}
 	
+	// 2° step incapsulamento: getter e setter
+	public int getBase() {
+		return base;
+	}
+	public int getAltezza() {
+		return altezza;
+	}
+	public void setBase(int base) {
+		this.base = base;
+	}
+	public void setAltezza(int altezza) {
+		this.altezza = altezza;
+	}
+	
+	//metodo per calcolare l'area 
 	public int calcolaArea() {
 		int area = this.base * this.altezza;
 		return area;
 	}
+	
+	//metodo per calcolare il perimetro 
 	public int calcolaPerimetro() {
 		int perimetro = (this.base + this.altezza) * 2;
 		return perimetro;
 	}
-		
-	 public void disegna () {
+	
+	//metodo per disegnare il rettangolo in console
+	public void disegna () {
 		 for (int i = 0; i < this.altezza; i++) {
 			 for (int x = 0; x < this.base; x++) {
 				 if(i == 0 || i == this.altezza -1 || x == 0 || x == this.base - 1) {
